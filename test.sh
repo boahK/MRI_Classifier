685c7bc1560fcd67cc05cc41e30271d9c1bfc49a
Boah Kim <56874068+boahK@users.noreply.github.com>
2024-08-27 16:24:36 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 python3 test.py \
 --path_data ./data/preprocessed \
---path_checkpoint ./pretrained_model \
+--path_checkpoint ./pretrained_model/DenseNet121_3d_classifier_finetuned \
 --path_result ./results \
 --model DenseNet121 
